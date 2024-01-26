a0a8de47e106abec2e980d89cd9cbce64ab5ea8f
DavideDePaoli98 <101572151+DavideDePaoli98@users.noreply.github.com>
2024-01-26 19:39:32 +0100

    Function Parameter changings
    The parameters given to the different functions are now called as elements of 'parameters'.

--- simulation_example.py
+++ simulation_example.py
@@ -6,22 +6,22 @@ parameters=fp.parameters_setting()
 
 
 ''' Secondly, the funcion bar-build generates the initial configuration of the array representing the bar.''' 
-initial_bar = fp.bar_builder(temperature_left,temperature_right,temperature_bar)
+initial_bar = fp.bar_builder(parameters['temperature_left'],parameters['temperature_right'],parameters['temperature_bar'])
 
 ''' Third, the functions correlated to the finite difference methods are applied on the initial bar to obtain the evolution of the temperature along the bar in time.
 In this case, the method DuFortFrankel is selected to compute the develop of the simulation.'''
-final_bar   = fp.DFF(lenght,time,initial_bar)
+final_bar   = fp.DFF(parameters['lenght'],parameters['time'],initial_bar)
 
 # Then, it is possible to work on the representation of the simulation. 
 
 ''' The function graph_time_t permits to visualize the temperature profile in one istant of the simulation;
 in this case, the istant chosen correspond to the entire time of the simulation over 2. '''
-fp.graph_time_t(lenght,time,time/2,final_bar)
+fp.graph_time_t(parameters['lenght'],parameters['time'],parameters['time']/2,final_bar)
 
 '''The function plot_3d represents the entire evolution of the bar temperature profile during the simulation time,
 by a reproduction of a 3D plot (Time vs Space vs Temperature).'''
-fp.plot_3D (lenght,time,final_bar)
+fp.plot_3D (parameters['lenght'],parameters['time'],final_bar)
 
 ''' The function plot_evolution represents the entire evolution of the bar temperature profile during the simulation time by an animation;
 the clock permits to visualize the istant of each profile. '''
-fp.plot_evolution(lenght,time,final_bar)
+fp.plot_evolution(parameters['lenght'],parameters['time'],final_bar)
