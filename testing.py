d672f3440e96cf2ebded42d210c5d0644dcfe44d
DavideDePaoli98 <101572151+DavideDePaoli98@users.noreply.github.com>
2024-01-29 16:00:11 +0100

    Some correction to make the code more comprehensive

--- testing.py
+++ testing.py
@@ -1,14 +1,21 @@
 from DiffEqLibrary import DiffEqLibrary_ as fp
 import numpy as np
 
-# Test to control if the return of the bar builder have the desired dimension and have the fixed temperatures in the extremetes 
-# (and in the well, if it exists) during the entire simulation time
+
 def test_bar_builder(temperature_left,temperature_right,temperature_bar):
+    
+    '''Test to control if the return of the bar builder have fixed temperatures in the extremetes and
+    along the initial bar (considering all the simulation time).'''
+    
     bar=fp.bar_builder (temperature_left,temperature_right,temperature_bar)
     assert all(i == temperature_right for i in bar[0,:])
     assert all(i == temperature_left for i in bar[-1,:])
     assert all(i == temperature_bar for i in bar[1:-1,0]) 
 def test_bar_builder_well(temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght):
+    
+    '''Test to control if the return of the bar builder have fixed temperatures in the extremetes,
+    in the well and along the initial bar (considering all the simulation time).'''
+    
     bar=fp.bar_builder_well (temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     well_position=int(well_position/lenght*bar.shape[0])
     assert all(i == temperature_right for i in bar[0,:])
@@ -17,9 +24,12 @@ def test_bar_builder_well(temperature_left,temperature_right,temperature_bar,wel
     assert all(i == temperature_bar for i in bar[1:well_position,0])
     assert all(i == temperature_bar for i in bar[well_position+1:-1,0])
 
-# Test to control if the return of the finite difference method functions have the desired dimension and have the fixed temperatures in the extremetes 
-# (and in the well, if it exists) during the entire simulation time. It also controls if the return is different from the initial bar configuration.
+
 def test_DFF(temperature_left,temperature_right,temperature_bar,lenght,time):
+
+    '''Test to control if the return of the finite difference method functions have fixed temperatures in the extremetes 
+    during the entire simulation time. It also controls if the return is different from the initial bar configuration.'''
+    
     initial_bar = fp.bar_builder (temperature_left,temperature_right,temperature_bar)
     final_bar = fp.DFF(lenght,time,np.copy(initial_bar))
     assert all(i == temperature_right for i in final_bar[0,:])
@@ -27,6 +37,10 @@ def test_DFF(temperature_left,temperature_right,temperature_bar,lenght,time):
     assert all(i == temperature_bar for i in final_bar[1:-1,0])
     assert not np.array_equal(initial_bar,final_bar)
 def test_DFF_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
+    
+    '''Test to control if the return of the finite difference method functions have fixed temperatures in the extremetes
+    and in the well during the entire simulation time. It also controls if the return is different from the initial bar configuration.'''
+    
     initial_bar = fp.bar_builder_well (temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     final_bar = fp.DFF_well(lenght,time,np.copy(initial_bar),well_position)
     well_position=int(well_position/lenght*initial_bar.shape[0])
@@ -38,12 +52,20 @@ def test_DFF_well(temperature_left,temperature_right,temperature_bar,lenght,time
     assert not np.array_equal(initial_bar,final_bar)
 
 def test_C_N(temperature_left,temperature_right,temperature_bar,lenght,time):
+    
+    '''Test to control if the return of the finite difference method functions have fixed temperatures in the extremetes 
+    during the entire simulation time. It also controls if the return is different from the initial bar configuration.'''
+    
     initial_bar = fp.bar_builder (temperature_left,temperature_right,temperature_bar)
     final_bar = fp.C_N(lenght,time,np.copy(initial_bar))
     assert all(i == temperature_right for i in final_bar[0,:])
     assert all(i == temperature_left for i in final_bar[-1,:])
     assert not np.array_equal(initial_bar,final_bar)
 def test_C_N_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
+    
+    '''Test to control if the return of the finite difference method functions have fixed temperatures in the extremetes
+    and in the well during the entire simulation time. It also controls if the return is different from the initial bar configuration.'''
+    
     initial_bar = fp.bar_builder_well (temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     final_bar = fp.C_N_well(lenght,time,np.copy(initial_bar),well_position)
     well_position=int(well_position/lenght*initial_bar.shape[0])
@@ -53,12 +75,20 @@ def test_C_N_well(temperature_left,temperature_right,temperature_bar,lenght,time
     assert not np.array_equal(initial_bar,final_bar)
 
 def test_R_K(temperature_left,temperature_right,temperature_bar,lenght,time):
+    
+    '''Test to control if the return of the finite difference method functions have fixed temperatures in the extremetes 
+    during the entire simulation time. It also controls if the return is different from the initial bar configuration.'''
+    
     initial_bar = fp.bar_builder (temperature_left,temperature_right,temperature_bar)
     final_bar = fp.R_K(lenght,time,np.copy(initial_bar))
     assert all(i == temperature_right for i in final_bar[0,:])
     assert all(i == temperature_left for i in final_bar[-1,:])
     assert not np.array_equal(initial_bar,final_bar)
 def test_R_K_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
+    
+    '''Test to control if the return of the finite difference method functions have fixed temperatures in the extremetes
+    and in the well during the entire simulation time. It also controls if the return is different from the initial bar configuration.'''
+    
     initial_bar = fp.bar_builder_well (temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     final_bar = fp.R_K_well(lenght,time,np.copy(initial_bar),well_position)
     well_position=int(well_position/lenght*initial_bar.shape[0])
@@ -68,12 +98,20 @@ def test_R_K_well(temperature_left,temperature_right,temperature_bar,lenght,time
     assert not np.array_equal(initial_bar,final_bar)
 
 def test_FTCS(temperature_left,temperature_right,temperature_bar,lenght,time):
+    
+    '''Test to control if the return of the finite difference method functions have fixed temperatures in the extremetes 
+    during the entire simulation time. It also controls if the return is different from the initial bar configuration.'''
+    
     initial_bar = fp.bar_builder (temperature_left,temperature_right,temperature_bar)
     final_bar = fp.FTCS(lenght,time,np.copy(initial_bar))
     assert all(i == temperature_right for i in final_bar[0,:])
     assert all(i == temperature_left for i in final_bar[-1,:])
     assert not np.array_equal(initial_bar,final_bar)
 def test_FTCS_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
+    
+    '''Test to control if the return of the finite difference method functions have fixed temperatures in the extremetes
+    and in the well during the entire simulation time. It also controls if the return is different from the initial bar configuration.'''
+    
     initial_bar = fp.bar_builder_well (temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     final_bar = fp.FTCS_well(lenght,time,np.copy(initial_bar),well_position)
     well_position=int(well_position/lenght*initial_bar.shape[0])
@@ -82,39 +120,44 @@ def test_FTCS_well(temperature_left,temperature_right,temperature_bar,lenght,tim
     assert all(i == temperature_well for i in final_bar[well_position,:])
     assert not np.array_equal(initial_bar,final_bar)
 
-
-
-#Test to control 4 different specific cases
  
 def test_case1(temperature_left,temperature_right,temperature_bar,lenght,time):
+    
     '''If the left thermostat temperature , the right thermostat temperature and the initial bar temperature are equals, 
     it is expected that the final temperature profile is equal to the initial one and that the temperature along the bar 
     is always equal to the temperature_left'''
+    
     bar = fp.bar_builder(temperature_left,temperature_right,temperature_bar)
     final_bar = fp.DFF(lenght,time,np.copy(bar))
     assert np.all(final_bar == temperature_left)
     assert np.array_equal(bar,final_bar)
 def test_case1_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
+    
     '''If the the left thermostat temperature , the right thermostat temperature, the initial bar temperature 
     and the well temperature are equals, it is expected that the final temperature profile is equal to the initial 
     one and that the temperature along the bar is always equal to the temperature_left'''
+    
     bar_well = fp.bar_builder_well(temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     final_bar_well = fp.DFF_well(lenght,time,np.copy(bar_well),well_position)
     assert np.all(final_bar_well == temperature_left)
     assert np.array_equal(bar_well,final_bar_well)
 
 def test_case2(temperature_left,temperature_right,temperature_bar,lenght,time):
+    
     '''If the left  and the right thermostat temperatures are higher than the bar temperature, 
     it is expected that the final temperature profile values are always lower or equal than the thermostat 
     temperatures.'''
+    
     bar = fp.bar_builder(temperature_left,temperature_right,temperature_bar)
     final_bar = fp.DFF(lenght,time,np.copy(bar))
     assert np.all(final_bar <= temperature_left)
     assert np.all(final_bar <= temperature_right)
 def test_case2_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
+    
     '''If the left  and the right thermostat temperatures are higher than the bar temperature and the well temperature, 
     it is expected that the final temperature profile values are always lower or equal than the thermostat 
     temperatures and higher than the well temperature.'''
+    
     bar_well = fp.bar_builder_well(temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     final_bar_well = fp.DFF_well(lenght,time,np.copy(bar_well),well_position)
     assert np.all(final_bar_well <= temperature_left)
@@ -122,20 +165,22 @@ def test_case2_well(temperature_left,temperature_right,temperature_bar,lenght,ti
     assert np.all(final_bar_well >= temperature_well)
 
 def test_case3(temperature_left,temperature_right,temperature_bar,lenght,time):
+    
     '''If the left thermostat temperature is higher than the bar temperature, and if the right thermostat 
     temperature is lower than the bar temperature, the temperature profile will decreases going from the left 
-    to the right.
-    '''
+    to the right.'''
+    
     bar = fp.bar_builder(temperature_left,temperature_right,temperature_bar)
     final_bar = fp.DFF(lenght,time,np.copy(bar))
     assert np.all(final_bar <= temperature_left)
     assert np.all(final_bar >= temperature_right)
     assert all(i <= i+1 for i in final_bar[:,-1])
 def test_case3_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
+    
     '''If the left thermostat temperature is higher than the bar temperature and the well temperature, 
     and if the right thermostat temperature is lower than the bar temperature and the well temperature, 
-    the temperature profile will decreases going from the left to the right.
-    '''
+    the temperature profile will decreases going from the left to the right.'''
+    
     bar_well = fp.bar_builder_well(temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     final_bar_well = fp.DFF_well(lenght,time,np.copy(bar_well),well_position)
     assert np.all(final_bar_well <= temperature_left)
@@ -143,10 +188,11 @@ def test_case3_well(temperature_left,temperature_right,temperature_bar,lenght,ti
     assert all(i <= i+1 for i in final_bar_well[:,-1])
 
 def test_case4_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
+    
     '''If the temperature of the left thermostat is equal to the temperature of the well and of the bar, 
     and it is higher than the temperature of the right thermostat, the temperature profile will be constant
-    from the left thermostat to the well, and than decreasing till the right thermostat.
-    '''
+    from the left thermostat to the well, and than decreasing till the right thermostat.'''
+    
     bar_well = fp.bar_builder_well(temperature_left,temperature_right,temperature_bar,well_position,temperature_well,lenght)
     final_bar_well = fp.DFF_well(lenght,time,np.copy(bar_well),well_position)
     well_position=int(well_position/lenght*bar_well.shape[0])
@@ -154,25 +200,8 @@ def test_case4_well(temperature_left,temperature_right,temperature_bar,lenght,ti
     assert all(final_bar_well[well_position+i,-1] >= final_bar_well[well_position+i+1,-1] for i in range(bar_well.shape[0]-well_position-1))
 
 
-test_case1(300,300,300,1,10)
-test_case1_well(300,300,300,1,10,0.5,300)
-test_case2(300,300,200,1,10)
-test_case2_well(300,300,200,1,10,0.5,170)
-test_case3(300,200,250,1,10)
-test_case3_well(300,200,250,1,10,0.5,230)
-test_case4_well(200,300,300,1,10,0.5,300)
-
-
-
-
-
-
-
-
-
-
-
 #Test to control if the different functions work well with general initial conditions
+
 test_bar_builder(100,200,300)
 test_bar_builder_well(100,200,300,0.5,250,1)
 test_DFF(100,200,300,1,10)
@@ -183,3 +212,13 @@ test_R_K(100,200,300,1,10)
 test_R_K_well(100,200,300,1,10,0.5,250)
 test_FTCS(100,200,300,1,10)
 test_FTCS_well(100,200,300,1,10,0.5,250)
+
+#Test to verify the programme results in specific cases
+
+test_case1(300,300,300,1,10)
+test_case1_well(300,300,300,1,10,0.5,300)
+test_case2(300,300,200,1,10)
+test_case2_well(300,300,200,1,10,0.5,170)
+test_case3(300,200,250,1,10)
+test_case3_well(300,200,250,1,10,0.5,230)
+test_case4_well(200,300,300,1,10,0.5,300)
