280414d26f8e19ff3ceebc0eb1044f1ffe977da6
DavideDePaoli98 <101572151+DavideDePaoli98@users.noreply.github.com>
2024-01-29 14:52:47 +0100

    Development of the DFF test
    Removing of the for cycles in the DFF test functions, mantaining the same goals.

--- testing.py
+++ testing.py
@@ -22,30 +22,19 @@ def test_bar_builder_well(temperature_left,temperature_right,temperature_bar,wel
 def test_DFF(temperature_left,temperature_right,temperature_bar,lenght,time):
     initial_bar = fp.bar_builder (temperature_left_=temperature_left,temperature_right_=temperature_right,temperature_bar_=temperature_bar,dim_X_=dim_X,dim_t_=dim_t)
     final_bar = fp.DFF(lenght,time,initial_bar,linear_diffusion)
-    assert final_bar.shape[0] == dim_X
-    assert final_bar.shape[1] == dim_t
-    for m in range(dim_X):
-        if m != 0 and m != dim_X-1:
-            assert final_bar[m][0] == temperature_bar
-            assert final_bar[m][1] == temperature_bar
-    for n in range(dim_t):
-        assert final_bar[0][n]  == temperature_right
-        assert final_bar[-1][n] == temperature_left
+    assert all(i == temperature_right for i in final_bar[0,:])
+    assert all(i == temperature_left for i in final_bar[-1,:])
+    assert all(i == temperature_bar for i in final_bar[1:-1,0])
     assert not np.array_equal(initial_bar,final_bar)
 def test_DFF_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
     initial_bar = fp.bar_builder_well (temperature_left_=temperature_left,temperature_right_=temperature_right,temperature_bar_=temperature_bar,well_position_=well_position,temperature_well_=temperature_well,lenght_=lenght,dim_X_=dim_X,dim_t_=dim_t)
     final_bar = fp.DFF_well(lenght,time,initial_bar,well_position,linear_diffusion)
-    well_position=int(well_position/lenght*dim_X)
-    assert final_bar.shape[0]==dim_X
-    assert final_bar.shape[1]==dim_t
-    for m in range(dim_X):
-        if m != 0 and m != dim_X-1 and m != well_position:
-            assert final_bar[m][0] == temperature_bar
-            assert final_bar[m][1] == temperature_bar
-    for n in range(dim_t):
-        assert final_bar[0][n]             == temperature_right
-        assert final_bar[-1][n]            == temperature_left
-        assert final_bar[well_position][n] == temperature_well
+    well_position=int(well_position/lenght*initial_bar.shape[0])
+    assert all(i == temperature_right for i in final_bar[0,:])
+    assert all(i == temperature_left for i in final_bar[-1,:])
+    assert all(i == temperature_well for i in final_bar[well_position,:])
+    assert all(i == temperature_bar for i in final_bar[1:well_position,0])
+    assert all(i == temperature_bar for i in final_bar[well_position+1:-1,0])
     assert not np.array_equal(initial_bar,final_bar)
 def test_C_N(temperature_left,temperature_right,temperature_bar,lenght,time):
     initial_bar = fp.bar_builder (temperature_left_=temperature_left,temperature_right_=temperature_right,temperature_bar_=temperature_bar,dim_X_=dim_X,dim_t_=dim_t)
