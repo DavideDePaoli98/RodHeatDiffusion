c424e07948e38491bd92873cb6ab918146778934
DavideDePaoli98 <101572151+DavideDePaoli98@users.noreply.github.com>
2024-01-29 14:57:56 +0100

    Development of the FTCS functions
    Removing of the for cycles in the FTCS functions, mantaining the same goals regarding the different asserts.

--- testing.py
+++ testing.py
@@ -67,28 +67,16 @@ def test_R_K_well(temperature_left,temperature_right,temperature_bar,lenght,time
 def test_FTCS(temperature_left,temperature_right,temperature_bar,lenght,time):
     initial_bar = fp.bar_builder (temperature_left_=temperature_left,temperature_right_=temperature_right,temperature_bar_=temperature_bar,dim_X_=dim_X,dim_t_=dim_t)
     final_bar = fp.FTCS(lenght,time,initial_bar,linear_diffusion)
-    assert final_bar.shape[0] == dim_X
-    assert final_bar.shape[1] == dim_t
-    for m in range(dim_X):
-        if m != 0 and m != dim_X-1:
-            assert final_bar[m][0] == temperature_bar
-    for n in range(dim_t):
-        assert final_bar[0][n]  == temperature_right
-        assert final_bar[-1][n] == temperature_left
+    assert all(i == temperature_right for i in final_bar[0,:])
+    assert all(i == temperature_left for i in final_bar[-1,:])
     assert not np.array_equal(initial_bar,final_bar)
 def test_FTCS_well(temperature_left,temperature_right,temperature_bar,lenght,time,well_position,temperature_well):
     initial_bar = fp.bar_builder_well (temperature_left_=temperature_left,temperature_right_=temperature_right,temperature_bar_=temperature_bar,well_position_=well_position,temperature_well_=temperature_well,lenght_=lenght,dim_X_=dim_X,dim_t_=dim_t)
     final_bar = fp.FTCS_well(lenght,time,initial_bar,well_position,linear_diffusion)
-    well_position=int(well_position/lenght*dim_X)
-    assert final_bar.shape[0]==dim_X
-    assert final_bar.shape[1]==dim_t
-    for m in range(dim_X):
-        if m != 0 and m != dim_X-1 and m != well_position:
-            assert final_bar[m][0] == temperature_bar
-    for n in range(dim_t):
-        assert final_bar[0][n]             == temperature_right
-        assert final_bar[-1][n]            == temperature_left
-        assert final_bar[well_position][n] == temperature_well
+    well_position=int(well_position/lenght*initial_bar.shape[0])
+    assert all(i == temperature_right for i in final_bar[0,:])
+    assert all(i == temperature_left for i in final_bar[-1,:])
+    assert all(i == temperature_well for i in final_bar[well_position,:])
     assert not np.array_equal(initial_bar,final_bar)
 
 #Test to control the different functions returns considering variation about the lenght of the bar and the time interval of the simulation
